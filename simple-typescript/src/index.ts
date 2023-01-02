37716d1c5d0a00406ec26ac5fefe10d65ee7839e
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-02 17:29:20 -0600

    Experimenting with union types

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,63 +1,54 @@
-// Generic Functions
-function genericFunction<T>(x: T): T {
-    return x;
-  }
-  
-  const genericArrowFunction = <T>(x: T): T => x;
-  
-  // Generic Interfaces
-  interface GenericInterface<T> {
-    (a: T): T;
-    someProp: T;
-  }
-  
-  interface GenericInterface<T> {
-    <U>(a: U): U;
-    someProp: T;
-  }
-  
-  // Generic Classes
-  class GenericClass<P> {
-    constructor(public props: P) {}
-  
-    getProps(): P {
-      return this.props;
+// Part 1
+function someFn(myArgument: number | string | boolean) {
+    if (typeof myArgument === 'string') {
+      let x = myArgument.toLocaleUpperCase();
+    } else if (typeof myArgument === 'number') {
+      myArgument.toFixed();
+    } else {
+      myArgument;
     }
   }
   
-  interface Expirable {
-    expiryDate: Date;
-  }
-  interface ChocolateCake extends Expirable {}
-  interface VanillaCake extends Expirable {}
-  
-  const chocoCakes: ChocolateCake[] = [{ expiryDate: new Date() }];
-  const vanillaCakes: VanillaCake[] = [{ expiryDate: new Date() }];
-  
-  interface GetExpiredItemsFunction {
-    <Item extends Expirable>(items: Array<Item>): Array<Item>;
+  // Part 2
+  interface Dog {
+    bark(): void;
+    walk(): void;
   }
   
-  const getExpiredItems: GetExpiredItemsFunction = items => {
-    const currentDate = new Date().getTime();
-    return items.filter(item => item.expiryDate.getDate() < currentDate);
-  };
+  interface Cat {
+    meow(): void;
+    walk(): void;
+  }
   
-  const expiredChocoCakes = getExpiredItems<ChocolateCake>(chocoCakes);
-  const expiredVanillaCakes = getExpiredItems<VanillaCake>(vanillaCakes);
+  function isDog(someObj: Dog | Cat): someObj is Dog {
+    return typeof (<Dog>someObj).bark !== 'undefined';
+  }
   
-  class ShoppingCart<ItemId, Item extends { id: ItemId }> {
-    private items: Array<Item> = [];
-    addItem(item: Item) {
-      this.items.push(item);
+  function callMyPet(pet: Dog | Cat) {
+    pet.walk();
+    if (isDog(pet)) {
+      pet.bark();
+    } else {
+      pet.meow();
     }
-    getItemById(id: ItemId) {
-      this.items.find(item => item.id === id);
+  }
+  
+  // Part 3
+  class Foo {
+    foo: number = 0;
+    commonProp: string = '';
+  }
+  
+  class Bar {
+    bar: number = 0;
+    commonProp: string = '';
+  }
+  
+  function fooBarFunction(obj: Foo | Bar) {
+    if (obj instanceof Foo) {
+      obj.foo;
+    } else {
+      obj.bar;
     }
-    //
-    //  static x: Item;
-    //  static someMethod(x: Item) {
-    //    let y: Item;
-    //  }
   }
   
