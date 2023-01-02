aa46c5b1fd0b8966b9a20ecc0a7ff118b0f97bb2
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-02 14:02:23 -0600

    Experimenting with generics

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,44 +1,63 @@
-interface Animal {
-    name: string;
-    group: string | undefined;
-    setGroup(group: string): void;
+// Generic Functions
+function genericFunction<T>(x: T): T {
+    return x;
   }
   
-  class Cat implements Animal {
-    name: string;
-    group: string | undefined;
-    constructor(name: string, group?: string) {
-      this.name = name;
+  const genericArrowFunction = <T>(x: T): T => x;
+  
+  // Generic Interfaces
+  interface GenericInterface<T> {
+    (a: T): T;
+    someProp: T;
+  }
+  
+  interface GenericInterface<T> {
+    <U>(a: U): U;
+    someProp: T;
+  }
+  
+  // Generic Classes
+  class GenericClass<P> {
+    constructor(public props: P) {}
+  
+    getProps(): P {
+      return this.props;
     }
-    setGroup(group: string) {
-      this.group = group;
+  }
+  
+  interface Expirable {
+    expiryDate: Date;
+  }
+  interface ChocolateCake extends Expirable {}
+  interface VanillaCake extends Expirable {}
+  
+  const chocoCakes: ChocolateCake[] = [{ expiryDate: new Date() }];
+  const vanillaCakes: VanillaCake[] = [{ expiryDate: new Date() }];
+  
+  interface GetExpiredItemsFunction {
+    <Item extends Expirable>(items: Array<Item>): Array<Item>;
+  }
+  
+  const getExpiredItems: GetExpiredItemsFunction = items => {
+    const currentDate = new Date().getTime();
+    return items.filter(item => item.expiryDate.getDate() < currentDate);
+  };
+  
+  const expiredChocoCakes = getExpiredItems<ChocolateCake>(chocoCakes);
+  const expiredVanillaCakes = getExpiredItems<VanillaCake>(vanillaCakes);
+  
+  class ShoppingCart<ItemId, Item extends { id: ItemId }> {
+    private items: Array<Item> = [];
+    addItem(item: Item) {
+      this.items.push(item);
     }
-  }
-  
-  class Dog implements Animal {
-    name: string;
-    group: string | undefined;
-    constructor(name: string) {
-      this.name = name;
+    getItemById(id: ItemId) {
+      this.items.find(item => item.id === id);
     }
-    setGroup(group: string) {
-      this.group = group;
-    }
+    //
+    //  static x: Item;
+    //  static someMethod(x: Item) {
+    //    let y: Item;
+    //  }
   }
-  
-  interface AnimalConstructor<T> {
-    new (name: string): T;
-  }
-  
-  function initializeAnimal<T extends Animal>(
-    Animal: AnimalConstructor<T>,
-    name: string
-  ) {
-    const animal = new Animal(name);
-    animal.setGroup('mammals');
-    return animal;
-  }
-  
-  const cat = initializeAnimal(Cat, 'Felix');
-  const dog = initializeAnimal(Dog, 'Ava');
   
