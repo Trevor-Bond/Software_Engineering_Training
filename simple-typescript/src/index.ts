399ae41a55ab79df2367060bae22ba1145fdf3bc
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-01 20:23:23 -0600

    Working with classes, methods, this, and constructors

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,21 +1,56 @@
-function sum(a: number, b?:number): number {
-  return a + (b || 0);
-}
+class Robot {
+  _color: string = '';
 
-sum(1);
-
-type MyFunc = (a: number, b: number) => number;
-const sum2: MyFunc = (a, b) => a + b;
-
-function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
-  return numbers.reduce((result, num) => result + num, 0);
-}
-
-function calcArea(width: number, height: number): number;
-function calcArea(length: number): number;
-function calcArea(...args: number[]): number {
-  if (args.length === 2) {
-    return args[0] * args[1];
+  private static availableColors = ['green', 'yellow'];
+  static isColorAvailable(color: string) {
+    return Robot.availableColors.includes(color);
   }
-  return Math.pow(args[0], 2);
+
+  constructor(protected _name: string) {}
+
+  askName() {
+    console.log(`My name is ${this.name}`);
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} moved ${distance} meters`);
+  }
+
+  set color(color: string) {
+    if (!Robot.isColorAvailable(color)) {
+      throw new Error(`Color ${color} is not available`);
+    }
+    this._color = color;
+  }
+
+  set name(value: string) {
+    this._name = 'PREFIX_' + value;
+  }
+
+  get name() {
+    return this._name + '_SUFFIX';
+  }
 }
+
+class FlyingRobot extends Robot {
+  private readonly jetpackSize: number;
+
+  constructor(name: string, jetpackSize: number) {
+    super(name);
+    this.jetpackSize = jetpackSize;
+  }
+
+  move(distance: number) {
+    console.log(`${this.name} is flying`);
+    super.move(distance);
+  }
+}
+
+const robot = new Robot('John');
+robot.askName();
+
+const flyingRobot = new FlyingRobot('Jim', 2);
+flyingRobot.move(10);
+console.log(`Flying robot's jetpack size is`, flyingRobot.name);
+flyingRobot.name = 'Kevin';
+console.log(`My name is ${flyingRobot.name}`);
