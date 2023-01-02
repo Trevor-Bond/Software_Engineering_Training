1d285f44f409666ad914bbf2702ee5a88e8b122f
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-01 19:43:02 -0600

    Learning about how to build functions, typing, and overloading

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,74 +1,21 @@
-// Boolean
-let b: boolean = true;
+function sum(a: number, b?:number): number {
+  return a + (b || 0);
+}
 
-// Number
-let num: number = 1 + 0b1 + 0o1 + 0x1;
+sum(1);
 
-// String
-const hello: string = "Hello";
-const world: string = "World";
-const helloWorld = `
-  ${hello}
-  ${world}
-`;
+type MyFunc = (a: number, b: number) => number;
+const sum2: MyFunc = (a, b) => a + b;
 
-// Null and Undefined
-let n: null = null;
-let u: undefined = undefined;
+function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
+  return numbers.reduce((result, num) => result + num, 0);
+}
 
-//let someNumber: number = null;
-let someNumber: number = 0;
-
-function uppercaseFirstLetter(str: string | null) {
-  if (str) {
-    return str[0].toUpperCase() + str.substr(1);
+function calcArea(width: number, height: number): number;
+function calcArea(length: number): number;
+function calcArea(...args: number[]): number {
+  if (args.length === 2) {
+    return args[0] * args[1];
   }
-}
-
-console.log(uppercaseFirstLetter("hello"));
-uppercaseFirstLetter(null);
-
-// Object
-type primitiveTypes = boolean | number | string | symbol | null | undefined;
-const myObj: object = new Map();
-
-// Void
-function log(message: string): void {
-  console.log(message);
-  //return 'a string';
-}
-
-// Array
-let array1: string[] = ["x", "y"];
-let array2: Array<string> = array1;
-
-// Tuple
-let tuple: [string, number] = ["str", 1];
-
-// Enum
-enum Color {
-  Red = "r",
-  Green = "g",
-  Blue = "b",
-}
-
-let myFavoriteColor: Color = Color.Blue;
-console.log(Color.Red, Color.Green, Color.Blue);
-console.log(Color["Red"]);
-
-// Any
-let ANY: any;
-ANY = "a string";
-ANY = 1;
-ANY = true;
-
-// Type Assertions
-const email =
-  typeof document !== "undefined" ? document.getElementById("email") : null;
-
-if (email) {
-  email.addEventListener("change", (e) => {
-    const input = <HTMLInputElement>e.currentTarget;
-    console.log(input.value);
-  });
+  return Math.pow(args[0], 2);
 }
