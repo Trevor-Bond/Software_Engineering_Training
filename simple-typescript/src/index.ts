e92ad5a5e1bb6e90fac7eceae2186102e0617a3e
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-08 12:19:14 -0600

    Experimenting with intersection types

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,54 +1,26 @@
-// Part 1
-function someFn(myArgument: number | string | boolean) {
-    if (typeof myArgument === 'string') {
-      let x = myArgument.toLocaleUpperCase();
-    } else if (typeof myArgument === 'number') {
-      myArgument.toFixed();
-    } else {
-      myArgument;
-    }
-  }
-  
-  // Part 2
-  interface Dog {
-    bark(): void;
-    walk(): void;
-  }
-  
-  interface Cat {
-    meow(): void;
-    walk(): void;
-  }
-  
-  function isDog(someObj: Dog | Cat): someObj is Dog {
-    return typeof (<Dog>someObj).bark !== 'undefined';
-  }
-  
-  function callMyPet(pet: Dog | Cat) {
-    pet.walk();
-    if (isDog(pet)) {
-      pet.bark();
-    } else {
-      pet.meow();
-    }
-  }
-  
-  // Part 3
-  class Foo {
-    foo: number = 0;
-    commonProp: string = '';
-  }
-  
-  class Bar {
-    bar: number = 0;
-    commonProp: string = '';
-  }
-  
-  function fooBarFunction(obj: Foo | Bar) {
-    if (obj instanceof Foo) {
-      obj.foo;
-    } else {
-      obj.bar;
-    }
-  }
-  
+interface IA {
+  a: number;
+}
+
+interface IB {
+  b: number;
+}
+
+interface IC {
+  c: number;
+}
+
+function X(obj: IA & IB & IC) {
+  return obj.a + obj.b + obj.c;
+}
+
+function combine<ObjA extends object, ObjB extends object>(
+  objA: ObjA,
+  objB: ObjB
+): ObjA & ObjB {
+  return { ...objA, ...objB };
+}
+
+const objA = { a: 1 };
+const objB = { b: 2 };
+const resultObj = combine(objA, objB);
