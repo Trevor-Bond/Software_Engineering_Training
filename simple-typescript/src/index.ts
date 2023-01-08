afbff695fa845076b92fbbc617cecb17a1e0e828
Trevor Bond <trevor.d.bond@gmail.com>
2023-01-08 12:27:46 -0600

    Example of a type alias

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,26 +1,8 @@
-interface IA {
+type Alias1 = string | string[] | null;
+type Alias2 = { a: number } & { b: number };
+type Alias3<T> = T[];
+
+type Alias4 = {
   a: number;
-}
-
-interface IB {
   b: number;
-}
-
-interface IC {
-  c: number;
-}
-
-function X(obj: IA & IB & IC) {
-  return obj.a + obj.b + obj.c;
-}
-
-function combine<ObjA extends object, ObjB extends object>(
-  objA: ObjA,
-  objB: ObjB
-): ObjA & ObjB {
-  return { ...objA, ...objB };
-}
-
-const objA = { a: 1 };
-const objB = { b: 2 };
-const resultObj = combine(objA, objB);
+};
